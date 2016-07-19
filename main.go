60aae49856599272acd28beb5293b29d64fdc5c7
Andrew Stuart <andrew.stuart2@gmail.com>
2016-07-19 12:10:51 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/0xAX/notificator"
+	"github.com/hpcloud/tail"
+)
+
+var (
+	file = flag.String("f", "-", "the file to open")
+	msg  = flag.String("msg", "Phrase Found", "the message notification title")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	not := notificator.New(notificator.Options{AppName: "grep-notify"})
+
+	if *file == "-" {
+		*file = "/dev/stdin"
+	}
+
+	f, err := tail.TailFile(*file, tail.Config{Follow: true})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for l := range f.Lines {
+		if strings.Contains(l.Text, flag.Args()[0]) {
+			err = not.Push(*msg, fmt.Sprintf("%s: %s", time.Now(), l.Text), "", notificator.UR_NORMAL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+	}
+}
